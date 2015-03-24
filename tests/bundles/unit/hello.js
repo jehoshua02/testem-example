c77b6ca90b43fd5fce021e141c81e92b76a5138b
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-03-24 01:44:57 -0600

    Wow. Webpack + Testem. Insane.
    That was really tricky but it works!
    
    So basically ...
    
    1. testem watches the src and tests/unit directories.
    2. testem runs webpack on test files when watched files change.
    3. webpack bundles each test file separately.
    4. testem runs tests after webpack finishes bundling.
    
    Why webpack? Because it bundles commonjs modules, keeping global scope nice
    and clean. Not only does it bundle modules, but will also allow us to
    easily preprocess test files the same way source files will be
    preprocessed.

---
+++ tests/bundles/unit/hello.js
@@ -0,0 +1,71 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	describe('hello', function () {
+	  var hello = __webpack_require__(1);
+
+	  it('returns "hello world"', function () {
+	    expect(hello()).toBe('hello world');
+	  });
+
+	  it('should say my name, say my name', function () {
+	    expect(hello('Joshua')).toBe('hello Joshua');
+	  });
+	});
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = function hello(name) {
+	  name = name || 'world'
+	  return 'hello ' + name;
+	};
+
+
+/***/ }
+/******/ ]);
