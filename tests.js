e590f7f58b608ff680c66592c11bdc961d2e3f3e
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-03-23 22:35:30 -0600

    Adds another test to see it all working

--- tests.js
+++ tests.js
@@ -3,3 +3,9 @@ describe('hello', function () {
     expect(hello()).toBe('hello world');
   });
 });
+
+describe('hello(myName)', function () {
+  it('should say my name, say my name', function () {
+    expect(hello('Joshua')).toBe('hello Joshua');
+  });
+});
